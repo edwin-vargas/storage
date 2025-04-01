bc1c03ebfa0906ee08cc33f191c8d7562b5104c9
edwin-vargas <edwin548vargas@gmail.com>
2025-04-01 01:32:30 -0600

    minor express and validation config --zod imported

--- server/main.js
+++ server/main.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const app = express()
 const PORT = process.env.PORT ?? 3344
+const crypto = require('node:crypto')
+
+const app = express()
+app.disable('x-powered-by')
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('<h1>Cloud Storage</h1><p>Pending name</p>')
